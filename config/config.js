b1a51c828f6d9102ca50397867c8e78169813bc7
suresh d <suresheerf@gmail.com>
2023-08-31 14:13:22 +0530

    added logging

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const fs = require("fs");
-const fsPromise = require("fs/promises");
+require('dotenv').config();
+const fs = require('fs');
+const fsPromise = require('fs/promises');
 
 const CONFIG = {};
 
@@ -10,10 +10,10 @@ CONFIG.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 CONFIG.JWT_COOKIE_EXPIRES_IN = process.env.JWT_COOKIE_EXPIRES_IN;
 CONFIG.JWT_SECRET = process.env.JWT_SECRET;
 CONFIG.HOST = process.env.HOST;
-CONFIG.DBURI = process.env.DBURI || "mongodb://127.0.0.1:27017";
+CONFIG.DBURI = process.env.DBURI || 'mongodb://127.0.0.1:27017';
 
 const checkDirectories = async () => {
-  const paths = ["./public"];
+  const paths = ['./public'];
   for (const path of paths) {
     if (!fs.existsSync(path)) {
       await fsPromise.mkdir(path, {
---
+++ utils/logger.js
@@ -0,0 +1,27 @@
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+
+const errorTransport = new winston.transports.DailyRotateFile({
+  filename: 'application-error-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  level: 'error',
+  maxSize: '20m',
+  maxFiles: '14d',
+});
+const combinedTransport = new winston.transports.DailyRotateFile({
+  filename: 'application-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  defaultMeta: { service: 'api-test-easy' },
+  transports: [new winston.transports.Console(), errorTransport, combinedTransport],
+});
+
+export default logger;
